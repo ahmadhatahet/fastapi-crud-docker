7c5fcdb47378003abcf5b1531d000876cf6f197d
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2023-10-07 12:51:41 +0200

    func migrate db

--- services.py
+++ services.py
@@ -1,4 +1,5 @@
 from engine import Base, engine, SessionLocal
+from sampleData import insert_sample_data
 
 
 def create_tables():
@@ -8,6 +9,13 @@ def get_db():
     db = SessionLocal()
     try: yield db
     finally: db.close()
+
+def migrate_db(db):
+    # ceate tables
+    create_tables()
+    # insert sample data
+    insert_sample_data(db)
+
 
 
 if __name__ == "__main__":
