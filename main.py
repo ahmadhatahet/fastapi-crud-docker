bf74d236617fdf41985e7d397181572f4c177d26
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2023-10-07 13:04:53 +0200

    change host to 0.0.0.0 for docker

--- main.py
+++ main.py
@@ -83,4 +83,4 @@ def update_flight(flight_id: int, flight: Flight):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=5400, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5400, reload=True)
