6b2dfcc87d333fed46f30c3273095f1a0115a2f8
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2023-10-07 12:47:22 +0200

    define flights table

--- schema.py
+++ schema.py
@@ -11,5 +11,17 @@ class Plane(Base):
     seats = Column(Integer)
     commission_date = Column(DateTime)
 
+
+class Flight(Base):
+    __tablename__ = "flights"
+
+    id = Column(Integer, primary_key=True, index=True)
+    plane_type_id = Column(Integer, index=True)
+    flight_code = Column(String(10), index=True)
+    depart_location = Column(String(150))
+    destination_location = Column(String(150))
+    depart_time = Column(DateTime)
+    arrival_time = Column(DateTime, nullable=True)
+
 if __name__ == "__main__":
     ...
