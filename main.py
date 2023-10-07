f9da26f37c5a7b674312d13063ba3c4c900b07a8
Ahmad Hatahet <ahmad.hatahet@hotmail.com>
2023-10-07 12:54:40 +0200

    fastapi basic app

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+from fastapi import FastAPI, HTTPException
+from engine import SessionLocal
+import services
 import uvicorn
-from fastapi import FastAPI
 
-
+# FastAPI application
 app = FastAPI(title="CRUD using Fastapi and Docker")
 
-
+# Start
 @app.get("/")
-def home_page():
-    return {"hello": "world"}
+def main():
+    return {
+        'title': 'this is an API to perform CRUD operations on MariaDB',
+    }
 
 
-if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=5400, reload=True)
+# CRUD operations
+@app.get("/migrate/")
+def migrate_db():
+    db = SessionLocal()
+    services.migrate_db(db)
+    return {'message': 'Created tables and inserted sample data successfully!'}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="localhost", port=5400, reload=True)
